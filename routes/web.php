63475e3814262825853ca6decf51e3edc293b98b
jjuandiego099 <jchaparro167@unab.edu.co>
2025-09-11 10:55:40 -0500

    rutas basico

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('products', function () {
+    return 'Listado de Productos';
+});
+Route::get('products/create', function () {
+    return 'Formulario de creacion de productos';
+});
+Route::get('products/{id}/{category}', function ($id,$category=null) {
+    if ($category ==null){
+        return 'Formulario de creacion de productos '.$id;
+
+    }else{
+        return 'Formulario de creacion de productos '.$id." de la categoria ".$category;
+    }
+    
+});

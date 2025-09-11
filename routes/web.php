02607a1e6a33d19ed9e39cf0a1c60ac0ea8a27ed
jjuandiego099 <jchaparro167@unab.edu.co>
2025-09-11 11:37:43 -0500

    grupo rutas basicas

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('products', [ProductController::class,"index"]);
 
-Route::get('products/create',[ProductController::class,"create"] );
-Route::get('products/{id}/{category}', [ProductController::class,"show"]);
+Route::profix('products')->controller(ProductController::class)->group(function () {
+
+    Route::get('/',"index");
+
+    Route::get('/create',"create");
+    Route::get('/{id}/{category}',"show");
+});

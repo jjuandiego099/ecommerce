98eb3a6a126ce7f74838fe0f4f1c9441df10e39a
jjuandiego099 <jchaparro167@unab.edu.co>
2025-09-11 12:42:28 -0500

    layout basico

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.app')
+@section('content')
     <form action="" method="post">
         <label for="name">Name:</label>
         <input type="text" name="name" id="">
@@ -24,5 +17,4 @@
         <input type="text" name="brand" id="">
     </form>
     
-</body>
-</html>
+    @endsection

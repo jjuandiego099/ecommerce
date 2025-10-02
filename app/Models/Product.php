58e05234f06b1c4e0684912324e66c9ddc07b729
jjuandiego099 <jchaparro167@unab.edu.co>
2025-10-02 12:27:11 -0500

    products factory

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    use HasFactory;
+    protected $table="products";
+    protected $fillable=['name','price','description','price','category_id','brand_id'];
+    public $timestamps =true;
 }

a14b751b891d9c2a702b98459c80aa62bd8c3369
jjuandiego099 <jchaparro167@unab.edu.co>
2025-11-14 18:46:01 -0500

    arreglo cache

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -13,19 +13,11 @@ class CategorySeeder extends Seeder
 
     public function run(): void
     {
-        $mycategory1= new Category();
-        $mycategory1->name='Electronics';
-        $mycategory1->save();
-        $mycategory2= new Category();
-        $mycategory2->name='Phones';
-        $mycategory2->save();
-        $mycategory3= new Category();
-        $mycategory3->name='Tv';
+         $mycategory3= new Category();
+        $mycategory3->name='Todas';
         $mycategory3->save();
 
         Category::factory(20)->create();
-        $mycategory3= new Category();
-        $mycategory3->name='Todas';
-        $mycategory3->save();
+       
     }
 }
